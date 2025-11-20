93884a8ec99c250af9bdca4d9a9302f53f5ac1d3
celiboy93 <97116174+celiboy93@users.noreply.github.com>
2025-11-20 14:20:02 +0630

    Delete fresh.config.ts

--- fresh.config.ts
+++
@@ -1,7 +0,0 @@
-// fresh.config.ts
-import { defineConfig } from "$fresh/server.ts";
-import tailwind from "$fresh/plugins/tailwind.ts";
-
-export default defineConfig({
-  plugins: [tailwind()],
-});
