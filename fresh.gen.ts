01d36b22c2eaae96c653547e77335934ec48485e
celiboy93 <97116174+celiboy93@users.noreply.github.com>
2025-11-20 13:56:45 +0630

    Create fresh.gen.ts

---
+++ fresh.gen.ts
@@ -0,0 +1,17 @@
+// fresh.gen.ts
+
+// NOTE: This file is usually auto-generated locally. We add it manually for Deno Deploy.
+
+import * as $0 from "./routes/admin/topup.tsx";
+import * as $1 from "./routes/balance.tsx";
+
+const manifest = {
+  routes: {
+    "./routes/admin/topup.tsx": $0,
+    "./routes/balance.tsx": $1,
+  },
+  islands: {},
+  baseUrl: import.meta.url,
+};
+
+export default manifest;
